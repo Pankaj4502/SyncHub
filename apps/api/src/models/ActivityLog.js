f278a2b295d96a668dd6c4346cba5ea4fd5d7542
Pankaj4502 <prampal6@gmail.com>
2026-07-27 05:11:08 +0530

    Fixed error in activity log

--- apps/api/src/models/ActivityLog.js
+++ apps/api/src/models/ActivityLog.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const activityLogSchema = new mongoose.Schema({
-    acton:{
+    action:{
         type: String,
         required: true,
         enum:['CREATED_TASK', 'CREATED_PROJECT' , 'UPDATED_TASK' , 'DELETED_TASK' , 'DELETED_PROJECT','ADDED_COMMENT']
